11dd43186b8145d21a6666ed1784dce7c63560a1
Florez98 <heidyyelitzaflorezvera98@gmail.com>
2024-08-17 00:00:41 -0500

    Creación modelo autores

---
+++ Katio_net.Data/Models/Authrors.cs
@@ -0,0 +1,10 @@
+namespace katio.Data.Models;
+
+    public class Authors{
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Cpountry { get; set; } = "";
+        public DateTime Birthdate  { get; set; }
+
+    }
